136121b6cc26080e243d6f600435c1b1c0b7e707
Nikolaos Kakouros <nkak@kth.se>
2018-08-16 19:17:18 +0200

    Converts file permissions to octal

--- scripts/lib/init.py
+++ scripts/lib/init.py
@@ -9,11 +9,11 @@ home = os.path.expanduser("~")
 InitializeDB(home + "/.config/honeyd/names")
 
 try:
-	os.chmod(home + "/.config/honeyd/names", 0666)
+	os.chmod(home + "/.config/honeyd/names", 0o666)
 except:
 	pass
 
 try:
-	os.chmod(home + "/.config/honeyd", 0777)
+	os.chmod(home + "/.config/honeyd", 0o777)
 except:
 	pass
